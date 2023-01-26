2bc51a777a0ba43bf9e383424899d5c5c90e7d62
krzysztofc-git <118632157+krzysztofc-git@users.noreply.github.com>
2023-01-26 06:36:40 +0100

    Improved Gallery page

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,60 +1,70 @@
 import React from 'react';
 function Gallery() {
   function GalleryCarousel() {
-    return (
-      <div id="carouselExampleCaptions" className="carousel slide" style={{'max-width': '720px', margin: 'auto'}}>
-        <div className="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
-        </div>
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img src="images/pizza/aurelien-lemasson-theobald-x00CzBt4Dfk-unsplash.jpg" className="d-block w-100" alt="First" />
+    const listOfPizzas = [
+      ["Neapolitan","images/pizza/nik-owens-40OJLYVWeeM-unsplash.jpg"], 
+      ["Bianca","images/pizza/jonas-kakaroto-zlKdLdMREtE-unsplash.jpg"],
+      ["Sicilian","images/pizza/ivan-torres-MQUqbmszGGM-unsplash.jpg"],
+      ["Ziti","images/pizza/carissa-gan-_0JpjeqtSyg-unsplash.jpg"],
+      ["New York","images/pizza/aurelien-lemasson-theobald-x00CzBt4Dfk-unsplash.jpg"]
+    ];
+
+    function CarouselItems(props) {
+      const items = [];
+      for (let index = props.begin_index; index < props.list.length; index++) {
+        items.push(
+          <div className="carousel-item">
+            <img src={listOfPizzas[index][1]} className="d-block w-100" alt={listOfPizzas[index][0]} />
             <div className="carousel-caption">
-              <h1>Pizza XYZ</h1>
-              <p>Swipe left/right or use arrow icons to see more</p>
+              <h1>{listOfPizzas[index][0]}</h1>
             </div>
           </div>
-          <div className="carousel-item">
-            <img src="images/pizza/carissa-gan-_0JpjeqtSyg-unsplash.jpg" className="d-block w-100" alt="2..." />
-            <div className="carousel-caption d-none d-md-block">
-              <h5>Second slide label</h5>
+        );
+      }
+      return <>{items}</>;
+    }
+
+    function CarouselButtons(props) {
+      const items = [];
+      for (let index = props.begin_index; index < props.list.length; index++) {
+        items.push(
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to={String(index)} aria-label={"Slide".concat(" ",String(index+1))}></button>
+        );
+      }
+      return <>{items}</>;
+    }
+
+    return (
+      <div className='row px-3 row-gap-5'>
+        <div className='col py-1'>
+          <div id="carouselExampleCaptions" className="carousel slide" style={{'max-width': '720px', margin: 'auto'}}>
+            <div className="carousel-indicators">
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+              <CarouselButtons list={listOfPizzas} begin_index={1} />
+            </div>
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img src={listOfPizzas[0][1]} className="d-block w-100" alt={listOfPizzas[0][0]} />
+                <div className="carousel-caption">
+                  <p>Swipe left/right or use arrow icons to see more</p>
+                  <h1>{listOfPizzas[0][0]}</h1>
+                </div>
+              </div>
+              
+              <CarouselItems list={listOfPizzas} begin_index={1} />
+              
               
             </div>
-          </div>
-          <div className="carousel-item">
-            <img src="images/pizza/ivan-torres-MQUqbmszGGM-unsplash.jpg" className="d-block w-100" alt="3..." />
-            <div className="carousel-caption d-none d-md-block">
-              <h5>Third slide label</h5>
-              
-            </div>
-          </div>
-          <div className="carousel-item">
-            <img src="images/pizza/jonas-kakaroto-zlKdLdMREtE-unsplash.jpg" className="d-block w-100" alt="4..." />
-            <div className="carousel-caption d-none d-md-block">
-              <h5>Third slide label</h5>
-              
-            </div>
-          </div>
-          <div className="carousel-item">
-            <img src="images/pizza/nik-owens-40OJLYVWeeM-unsplash.jpg" className="d-block w-100" alt="5..." />
-            <div className="carousel-caption d-none d-md-block">
-              <h5>Third slide label</h5>
-              
-            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Next</span>
+            </button>
           </div>
         </div>
-        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
       </div>
     );
   }

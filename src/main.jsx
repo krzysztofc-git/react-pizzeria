54639147e48c49438314119c5037af3d84877b6e
krzysztofc-git <118632157+krzysztofc-git@users.noreply.github.com>
2024-08-03 22:05:36 +0100

    Convert single page app to react router (client side)

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
+import Reservation from "./pages/Reservation.jsx";
+import Main from "./pages/Main.jsx";
+import Status from "./pages/Status.jsx";
+import Gallery from "./pages/Gallery.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Main />
+      },
+      {
+        path: "reservation",
+        element: <Reservation />
+      },
+      {
+        path: "status",
+        element: <Status />
+      },
+      {
+        path: "gallery",
+        element: <Gallery />
+      }
+    ]
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
